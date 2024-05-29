0cecc9dfd850dc711bae46808e294b1e46001746
stevechirayath <steve@theorymesh.com>
2024-05-29 12:52:13 -0400

    upgrade lottie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {LottieModule} from 'ngx-lottie';
+
 import player from 'lottie-web';
+import { provideLottieOptions } from 'ngx-lottie';
 
 export function playerFactory() {
   return player;
@@ -15,9 +18,14 @@ export function playerFactory() {
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, LottieModule.forRoot({ player: playerFactory })],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-    bootstrap: [AppComponent]
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+      provideLottieOptions({
+        player: () => import('lottie-web'),
+      })
+    ],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
 }
